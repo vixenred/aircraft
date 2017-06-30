d0ee2d7ff7a9d3eb0fbe610adc58e214326f6fd4
Olena Rakhman <rahman.alena@gmail.com>
2017-06-30 21:47:19 +0300

    кнопки + и -, правки в спрайтах

--- public/index.js
+++ public/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 let game = null;
 let turret = null;
 let sight = null;
@@ -9,6 +8,15 @@ let planesCollisionGroup = null;
 let planes = null;
 let rockets = null;
 let explosions = null;
+let buttonMinus = null;
+let buttonPlus = null;
+let textBet = null;
+
+const BET_INDEX_DEFAULT = 0;
+const BETS = [10, 20, 30, 50, 100];
+const BET_DEFAULT = BETS[BET_INDEX_DEFAULT];
+let betIndex = BET_INDEX_DEFAULT;
+let bet = BETS[betIndex];
 
 
 function preload()
@@ -21,7 +29,28 @@ function preload()
     game.load.image('rocket3', 'assets/images/Rocket3.png');
     game.load.image('rocket4', 'assets/images/Rocket4.png');
     game.load.image('plane', 'assets/images/BluePlane.png');
+    game.load.image('buttonMinus', 'assets/images/ButtonMinus.png');
+    game.load.image('buttonPlus', 'assets/images/ButtonPlus.png');
     game.load.spritesheet('explode', 'assets/images/explode.png', 128, 128);
+    game.load.bitmapFont('desyrel', 'assets/fonts/desyrel.png', 'assets/fonts/desyrel.xml');
+}
+
+function onBetMore()
+{
+    betIndex++;
+    if (betIndex > BETS.length - 1)
+        betIndex = BETS.length - 1;
+    bet = BETS[betIndex];
+    textBet.text = bet;
+}
+
+function onBetLess()
+{
+    betIndex--;
+    if (betIndex < 0)
+        betIndex = 0;
+    bet = BETS[betIndex];
+    textBet.text = bet;
 }
 
 function setupExplosion (explosion)
@@ -48,19 +77,29 @@ function hitPlane(rocketBody, planeBody)
     explode(planeBody.x, planeBody.y, 1);
 }
 
+function over()
+{
+    console.log('button over');
+}
+
 function onTap()
 {
-    let rocket = rockets.create(turret.x, turret.y, 'rocket');
 
+    let rocket = rockets.create(turret.x, turret.y, 'rocket');
+    let betScale = Math.sqrt(Math.sqrt(bet / BET_DEFAULT));
+    let scale = 75 * betScale / rocket.width;
+
+    rocket.customValues = {};
+    rocket.customValues.bet = bet;
     game.physics.p2.enable(rocket, true);
-    rocket.body.setRectangle(20, 60);
+    rocket.body.setRectangle(60, 20);
     rocket.body.setCollisionGroup(rocketsCollisionGroup);
     rocket.body.collides(planesCollisionGroup, hitPlane, this);
     rocket.anchor.set(0.5, 0.5);
-    rocket.scale.set(0.3);
+    rocket.scale.setTo(scale);
 
-    const angle = turret.rotation - game.math.degToRad(90);
-    const speed = 300;
+    const angle = turret.rotation;
+    const speed = 300+(bet*2);
 
     rocket.body.rotation = turret.rotation;
     rocket.body.dynamic = true;
@@ -96,13 +135,27 @@ function create()
 {
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.p2.setImpactEvents(true);
+
     game.add.image(0, 0, 'background');
-    turret = game.add.sprite(game.width * 0.5, game.height * 0.9, 'turret');
-    turret.anchor.set(0.5, 0.7416);
+
+    turret = game.add.sprite(game.width * 0.5, game.height * 0.85, 'turret');
+    turret.anchor.set(0.335, 0.5);
     turret.scale.set(0.25);
+
     sight = game.add.sprite(game.width * 0.5, game.height * 0.5, 'sight');
     sight.anchor.set(0.5, 0.5);
     sight.scale.set(0.7);
+
+    buttonMinus = game.add.button(game.width * 0.4, game.height * 0.95, 'buttonMinus', onBetLess, this);
+    buttonMinus.anchor.set(0.5, 0.5);
+    buttonMinus.scale.set(0.5);
+    buttonMinus.onInputOver.add(over, this);
+
+    buttonPlus = game.add.button(game.width * 0.6, game.height * 0.95, 'buttonPlus', onBetMore, this);
+    buttonPlus.anchor.set(0.5, 0.5);
+    buttonPlus.scale.set(0.5);
+    buttonPlus.onInputOver.add(over, this);
+
     game.input.onTap.add(onTap, this);
     game.time.events.loop(Phaser.Timer.SECOND*3, spawn, this);
     rocketsCollisionGroup = game.physics.p2.createCollisionGroup();
@@ -112,15 +165,17 @@ function create()
     explosions = game.add.group();
     explosions.createMultiple(30, 'explode');
     explosions.forEach(setupExplosion, this);
+
+    textBet = game.add.bitmapText(game.width * 0.5, game.height * 0.93, 'desyrel', bet, 64);
+    textBet.anchor.set(0.5);
 }
 
 function update()
 {
     let x = game.input.activePointer.clientX;
     let y = game.input.activePointer.clientY;
-    let angle = Math.atan2(y - turret.y, x - turret.x);
 
-    turret.rotation = angle + game.math.degToRad(90);
+    turret.rotation = Math.atan2(y - turret.y, x - turret.x);
     sight.x = x;
     sight.y = y;
 }
